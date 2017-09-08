73f2a961755bde813e9b549bb2990dc20d79a4b6
Rikharthu <uberviolence@gmail.com>
2017-09-08 10:01:03 +0300

    added image size

--- app/src/main/java/com/example/uberv/maptbookapidownloader/HtmlUtils.java
+++ app/src/main/java/com/example/uberv/maptbookapidownloader/HtmlUtils.java
@@ -3,5 +3,6 @@ package com.example.uberv.maptbookapidownloader;
 public abstract class HtmlUtils {
 
     public static final String STYLE_PADDING = "html{padding: 2cm 4cm 3cm 4cm;}";
-    public static final String STYLES = "<style>" + STYLE_PADDING + "</style>";
+    public static final String STYLE_IMAGE_SIZE = "html{max-width: 800px; max-height: 800px;}";
+    public static final String STYLES = "<style>" + STYLE_PADDING + STYLE_IMAGE_SIZE + "</style>";
 }
